2983604a777ef033856e2111913d33253df57cc7
Gabriel Gnoatto <gnoattogabriel100@gmail.com>
2026-03-23 16:18:17 -0300

    first commit

---
+++ src/test/java/com/gnoatto/Fornecedores/FornecedoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gnoatto.Fornecedores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FornecedoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
